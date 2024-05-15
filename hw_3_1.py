b9be44f43a6c55adda07bb195414db3f93921d44
VrdnAlex <167655856+VrdnAlex@users.noreply.github.com>
2024-05-15 20:37:20 +0300

    Update hw_3_1.py

--- hw_3_1.py
+++ hw_3_1.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-        date_input = datetime.strptime(date, "%Y-%m-%d").data()
-        now_date = datetime.today
+        date_input = datetime.strptime(date, "%Y-%m-%d").date()
+        now_date = datetime.today.date()
         diffence_day = now_date - date_input
         return diffence_day.days
     except ValueError:
         return ("Invalid date format")
 
 
-print(get_days_from_today(date))
+print(get_days_from_today("2024-03-10"))
