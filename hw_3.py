c0bc0440507a92d1c04c10709eb1b7e355f948c1
VrdnAlex <167655856+VrdnAlex@users.noreply.github.com>
2024-05-15 20:32:54 +0300

    Update hw_3.py

--- hw_3.py
+++ hw_3.py
@@ -1,13 +1,17 @@
-from random import randint, sample
+from random import randint
 
 def generate_unique_numbers(min, max, quantity):
-    if (1 <= min <= max <= 1000) and (quantity <= max - min + 1):
-        result_array = set()
-        while len(result_array) != quantity:
-            result_array.add(randint(min, max))
-        return sorted(list(result_array))
-    else:
+    if not (1 <= min <= max <= 1000) and (quantity <= max - min + 1):
         return []
+    result_array = set()
+    
+    while len(result_array) != quantity:
+            result_array.add(randint(min, max))
+    return sorted(list(result_array))
+        
+min = 1
+max = 49
+quantity = 6
 
 lottery_numbers = generate_unique_numbers(min, max, quantity)
 print("Your lottery numbers:", lottery_numbers)
