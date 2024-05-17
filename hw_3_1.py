7c421569098bc3ab1030ebf350de0dbe4ed594f0
VrdnAlex <167655856+VrdnAlex@users.noreply.github.com>
2024-05-17 16:50:10 +0300

    Update hw_3_1.py

--- hw_3_1.py
+++ hw_3_1.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def get_days_from_today(date):
     try:
         date_input = datetime.strptime(date, "%Y-%m-%d").date()
-        now_date = datetime.today.date()
+        now_date = datetime.today().date()
         diffence_day = now_date - date_input
         return diffence_day.days
     except ValueError:
